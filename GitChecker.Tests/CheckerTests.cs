6aea44022308d603ac75ec1964dbd2f6744ce91e
gammja <gammja@gmail.com>
2016-09-09 00:52:08 +0600

    done

---
+++ GitChecker.Tests/CheckerTests.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.IO;
+using LibGit2Sharp;
+using NUnit.Framework;
+
+namespace GitChecker.Tests
+{
+    [TestFixture]
+    public class CheckerTests
+    {
+        [Test]
+        public void IfMaster_Pass()
+        {
+            var repo = CreateRepository(); //master is head
+            CreateAndAddFile(repo);
+            MakeCommit(repo, DateTimeOffset.Now);
+            var checker = new GitChecker(repo);
+
+            //act
+            var isValid = checker.IsValid();
+
+            Assert.That(isValid, Is.True);
+        }
+
+        [Test]
+        public void IfDevelop_Pass()
+        {
+            var repo = CreateRepository(); //master
+            CreateAndAddFile(repo);
+            MakeCommit(repo, DateTimeOffset.Now);
+            repo.CreateBranch("develop");
+            repo.Checkout("develop"); //develop is head
+            CreateAndAddFile(repo);
+            MakeCommit(repo, DateTimeOffset.Now);
+            var checker = new GitChecker(repo);
+
+            //act
+            var isValid = checker.IsValid();
+
+            Assert.That(isValid, Is.True);
+        }
+
+        [Test]
+        public void IfCommintInDevelopToday_IsValid()
+        {
+            var repo = CreateRepository(); //master
+            CreateAndAddFile(repo);
+            MakeCommit(repo, DateTimeOffset.Now.AddDays(-2));
+            repo.CreateBranch("develop");
+            repo.Checkout("develop");
+            CreateAndAddFile(repo);
+            MakeCommit(repo, DateTimeOffset.Now.AddDays(-2));
+
+            repo.CreateBranch("feature/test");
+            repo.Checkout("feature/test"); //featru is head
+            CreateAndAddFile(repo);
+            MakeCommit(repo, DateTimeOffset.Now.AddDays(-1));
+
+            repo.Checkout("develop");
+            CreateAndAddFile(repo);
+            MakeCommit(repo, DateTimeOffset.Now);//today
+
+            repo.Checkout("feature/test");
+
+            var checker = new GitChecker(repo);
+
+            //act
+            var isValid = checker.IsValid();
+
+            Assert.That(isValid, Is.True);
+        }
+
+        [Test]
+        public void IfCommintInDevelopYesterday_IsInValid()
+        {
+            var repo = CreateRepository(); //master
+            CreateAndAddFile(repo);
+            MakeCommit(repo, DateTimeOffset.Now.AddDays(-2));
+            repo.CreateBranch("develop");
+            repo.Checkout("develop");
+            CreateAndAddFile(repo);
+            MakeCommit(repo, DateTimeOffset.Now.AddDays(-2));
+
+            repo.CreateBranch("feature/test");
+            repo.Checkout("feature/test");
+            CreateAndAddFile(repo);
+            var yesterday = DateTimeOffset.Now.AddDays(-1);
+            MakeCommit(repo, yesterday);
+
+            repo.Checkout("develop");
+            CreateAndAddFile(repo);
+            MakeCommit(repo, DateTimeOffset.Now.AddDays(-1));//yesterday
+
+            repo.Checkout("feature/test");
+
+            var checker = new GitChecker(repo);
+
+            //act
+            var isValid = checker.IsValid();
+
+            Assert.That(isValid, Is.False);
+        }
+
+        [Test]
+        public void AfterMerge_IsValid()
+        {
+            var repo = CreateRepository(); //master
+            CreateAndAddFile(repo);
+            MakeCommit(repo, DateTimeOffset.Now.AddDays(-2));
+            repo.CreateBranch("develop");
+            repo.Checkout("develop");
+            CreateAndAddFile(repo);
+            MakeCommit(repo, DateTimeOffset.Now.AddDays(-2));
+
+            repo.CreateBranch("feature/test");
+            repo.Checkout("feature/test");
+            CreateAndAddFile(repo);
+            MakeCommit(repo, DateTimeOffset.Now.AddDays(-1));
+
+            repo.Checkout("develop");
+            CreateAndAddFile(repo);
+            MakeCommit(repo, DateTimeOffset.Now); //merge
+
+            repo.Checkout("feature/test");
+            repo.Merge(repo.Branches["develop"], new Signature("test", "test@gmail.com", DateTimeOffset.Now));
+
+            var checker = new GitChecker(repo);
+
+            //act
+            var isValid = checker.IsValid();
+
+            Assert.That(isValid, Is.True);
+        }
+
+        private static void MakeCommit(Repository repo, DateTimeOffset dateTime)
+        {
+            var message = string.Format("{0} - test", repo.Head);
+            repo.Commit(message, new Signature("test", "test@gmail.com", dateTime));
+        }
+
+        private void CreateAndAddFile(Repository repo)
+        {
+            var relativeFileName = Guid.NewGuid().ToString();
+            var path = Path.Combine(repo.Info.WorkingDirectory, relativeFileName);
+
+            var contents = Guid.NewGuid().ToString();
+            File.WriteAllText(path, contents);
+
+            repo.Stage(path);
+        }
+
+        private static Repository CreateRepository()
+        {
+            var path = Repository.Init(Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString()));
+            return new Repository(path);
+        }
+    }
+}
---
+++ GitChecker/GitChecker.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using LibGit2Sharp;
+
+namespace GitChecker
+{
+    public class GitChecker
+    {
+        private readonly Repository _repo;
+
+        public GitChecker() : this(".") {}
+
+        public GitChecker(string repoPath)
+        {
+            if (string.IsNullOrWhiteSpace(repoPath))
+                throw new ArgumentException("Value cannot be null or whitespace.", nameof(repoPath));
+
+            _repo = new Repository(repoPath);
+        }
+
+        //for tests
+        internal GitChecker(Repository repo)
+        {
+            if (repo == null) throw new ArgumentNullException(nameof(repo));
+
+            _repo = repo;
+        }
+
+        public bool IsValid()
+        {
+            var develop = _repo.Branches["develop"];
+
+            var isFeature = _repo.Head.FriendlyName.Contains("feature/");
+            if (!isFeature
+                || develop == null
+                || develop.Tip.Author.When > DateTime.Today)
+            {
+                return true;
+            }
+
+            return _repo.Head.Commits
+                .SelectMany(c => c.Parents)
+                .Contains(develop.Tip);
+        }
+    }
+}
